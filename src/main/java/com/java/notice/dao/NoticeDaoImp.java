99180dd91d7d2122690d69bc903decb1a06a6eaa
Vulpes94 <kjunfi94@gmail.com>
2022-01-28 18:32:00 +0900

    feat: 공지사항,자주 묻는 질문,문의하기 View완성
    문의하기 insert까지 완료함.
    View에는 [추천수]가 있지만 디비 테이블에 [추천수]가 존재하지 않음. 수정 필요.

--- src/main/java/com/java/notice/dao/NoticeDaoImp.java
+++ src/main/java/com/java/notice/dao/NoticeDaoImp.java
@@ -2,9 +2,11 @@ package com.java.notice.dao;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.java.notice.dto.QuestionDto;
 
+@Component
 public class NoticeDaoImp implements NoticeDao {
 	@Autowired
 	private SqlSessionTemplate sqlSessionTemplate;
